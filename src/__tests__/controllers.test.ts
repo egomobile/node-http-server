0d6ee22be7ec90de8151ffd57fe45a4867e6295d
Marcel Kloubert <marcel.kloubert@e-go-mobile.com>
2021-09-02 09:24:08 +0200

    implement first controller tests

---
+++ src/__tests__/controllers.test.ts
@@ -0,0 +1,48 @@
+// This file is part of the @egomobile/http-server distribution.
+// Copyright (c) Next.e.GO Mobile SE, Aachen, Germany (https://e-go-mobile.com/)
+//
+// @egomobile/http-server is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation, version 3.
+//
+// @egomobile/http-server is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import path from 'path';
+import request from 'supertest';
+import { binaryParser, createServer } from './utils';
+
+function createServerAndInitControllers() {
+    const server = createServer();
+
+    server.controllers(path.join(__dirname, 'controllers'));
+
+    return server;
+}
+
+describe('controllers', () => {
+    it.each(['/', '/foo'])('should return 200 when do a GET request for existing IndexController', async (p) => {
+        const expectedResult = 'bar:' + p;
+
+        const server = createServerAndInitControllers();
+
+        const response = await request(server).get(p)
+            .send()
+            .parse(binaryParser)
+            .expect(200);
+
+        const data = response.body;
+        expect(Buffer.isBuffer(data)).toBe(true);
+
+        const str = data.toString('utf8');
+
+        expect(typeof str).toBe('string');
+        expect(str.length).toBe(expectedResult.length);
+        expect(str).toBe(expectedResult);
+    });
+});
---
+++ src/__tests__/controllers/index.ts
@@ -0,0 +1,15 @@
+import { Controller, IHttpRequest, IHttpResponse } from '../../../src';
+import { ControllerBase, GET } from '../../../src/controllers';
+
+@Controller()
+export default class IndexController extends ControllerBase {
+    @GET()
+    async index(request: IHttpRequest, response: IHttpResponse) {
+        response.write('bar:' + request.url);
+    }
+
+    @GET()
+    async foo(request: IHttpRequest, response: IHttpResponse) {
+        response.write('bar:' + request.url);
+    }
+}
